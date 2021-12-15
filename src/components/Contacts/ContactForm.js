112b1978e061ee017d10986b1fea959a243d1a2c
VictoriaSTo <78522147+VictoriaSTo@users.noreply.github.com>
2021-12-15 13:01:00 +0800

    added validation to contact form

--- src/components/Contacts/ContactForm.js
+++ src/components/Contacts/ContactForm.js
@@ -5,6 +5,7 @@ import { styled } from '@mui/material/styles';
 import theme from "../UI/Theme";
 
 import ContainedButtonEl from '../UI/ContainedButton';
+import useInput from '../../hooks/useInput';
 
 // Parses the JSON returned by a network request
 const parseJSON = resp => (resp.json ? resp.json() : resp);
@@ -21,6 +22,10 @@ const checkStatus = resp => {
 const headers = {
   'Content-Type': 'application/json',
 };
+
+// Validation options
+const isNotEmpty = (value) => value.trim() !== '';
+const isEmail = (value) => value.includes('@');
 
 // Style customization
 const InputTextField = styled(TextField)({
@@ -44,21 +49,52 @@ const InputTextField = styled(TextField)({
 });
 
 const ContactForm = () => {
-  const [enteredName, setEnteredName] = useState(null);
-  const [enteredEmail, setEnteredEmail] = useState(null);
-  const [enteredMessage, setEnteredMessage] = useState(null);
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetName,
+  } = useInput(isNotEmpty);
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+  const {
+    value: messageValue,
+    isValid: messageIsValid,
+    hasError: messageHasError,
+    valueChangeHandler: messageChangeHandler,
+    inputBlurHandler: messageBlurHandler,
+    reset: resetMessage,
+  } = useInput(isNotEmpty);
 
-  const submitFormHandler = async e => {
+  let formIsValid = false;
+
+  if (nameIsValid && emailIsValid && messageIsValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = async e => {
     e.preventDefault();
-    console.log(enteredMessage, enteredName, enteredEmail)
+
+    if (!formIsValid) {
+      return;
+    }
+
     try {
       await fetch('http://localhost:1337/contact-uses', {
         method: 'POST',
         headers: headers,
         body: JSON.stringify({
-          name: enteredName,
-          email: enteredEmail,
-          message: enteredMessage
+          name: nameValue,
+          email: emailValue,
+          message: messageValue
         }),
       })
         .then(checkStatus)
@@ -67,44 +103,36 @@ const ContactForm = () => {
       console.log(error)
     }
 
-    setEnteredName("");
-    setEnteredEmail("");
-    setEnteredMessage("");
+    resetName();
+    resetEmail();
+    resetMessage();
   }
-
-  const nameChangeHandler = (event) => {
-    setEnteredName(event.target.value);
-  }
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-  }
-  const messageChangeHandler = (event) => {
-    setEnteredMessage(event.target.value);
-  }
-
 
   return (
     <div data-aos-duration="1000" data-aos="fade-left" data-aos-once="true">
       <h3>Get In Touch</h3>
-      <form onSubmit={submitFormHandler}>
+      <form onSubmit={submitHandler}>
         <Grid container columnSpacing={6} rowSpacing={4} sx={{marginBottom: "5%"}}>
           <Grid item xs={12} md={6}>
-          <InputTextField required fullWidth id="Name" label="Name" variant="filled" size="small" value={enteredName} onChange={nameChangeHandler}/>
+          <InputTextField required fullWidth id="Name" label="Name" variant="filled" size="small" value={nameValue} onChange={nameChangeHandler} onBlur={nameBlurHandler} error={nameHasError} helperText={nameHasError ? "Please enter your name" : ""}/>
             </Grid>
             <Grid item xs={12} md={6}>
-              <InputTextField required fullWidth id="Email" label="Email" variant="filled" size="small" value={enteredEmail} onChange={emailChangeHandler}/>
+              <InputTextField required fullWidth id="Email" label="Email" variant="filled" size="small" value={emailValue} onChange={emailChangeHandler} onBlur={emailBlurHandler}  error={emailHasError}helperText={emailHasError ? "Please enter a valid email address" : ""}/>
             </Grid>
             <Grid item xs={12} md={12}>
               <InputTextField
                 required 
                 fullWidth 
+                error={messageHasError}
+                helperText={messageHasError ? "Message must be thirty or more characters" : ""}
                 id="Message"
                 label="Message"
                 multiline
                 variant="filled"
                 rows={4}
                 onChange={messageChangeHandler}
-                value={enteredMessage}
+                onBlur={messageBlurHandler}
+                value={messageValue}
               />
             </Grid>
         </Grid>
