58a1f45e930553c4d130e8a1337e78d0f44e9d30
VictoriaSTo <78522147+VictoriaSTo@users.noreply.github.com>
2021-12-15 12:16:33 +0800

    added form validation to application form

--- src/components/Application/ApplicationForm.js
+++ src/components/Application/ApplicationForm.js
@@ -6,58 +6,255 @@ import theme from "../UI/Theme";
 
 import ContainedButtonEl from '../UI/ContainedButton';
 import classes from './ApplicationForm.module.css';
-import applicationFieldsInfo from './applicationFieldsInfo';
+import useInput from '../../hooks/useInput';
 
-const InputTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: theme.palette.common.lightBlue,
-  },
-  "&:hover label": {
-    color: theme.palette.common.lightBlue
-  },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'white',
-    },
-    '&:hover fieldset': {
-      borderColor: theme.palette.common.lightBlue,
-    },
-    '&.Mui-focused fieldset': {
-      borderColor: theme.palette.common.lightBlue,
-    },
+const isNotEmpty = (value) => value.trim() !== '';
+const isEmail = (value) => value.includes('@');
+
+const ApplicationForm = (props) => {
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+  const {
+    value: phoneNumberValue,
+    isValid: phoneNumberIsValid,
+    hasError: phoneNumberHasError,
+    valueChangeHandler: phoneNumberChangeHandler,
+    inputBlurHandler: phoneNumberBlurHandler,
+    reset: resetPhoneNumber,
+  } = useInput(isNotEmpty);
+  const {
+    value: jobTitleValue,
+    isValid: jobTitleIsValid,
+    hasError: jobTitleHasError,
+    valueChangeHandler: jobTitleChangeHandler,
+    inputBlurHandler: jobTitleBlurHandler,
+    reset: resetJobTitle,
+  } = useInput(isNotEmpty);
+  const {
+    value: organizationValue,
+    isValid: organizationIsValid,
+    hasError: organizationHasError,
+    valueChangeHandler: organizationChangeHandler,
+    inputBlurHandler: organizationBlurHandler,
+    reset: resetOrganization,
+  } = useInput(isNotEmpty);
+  const {
+    value: countryValue,
+    isValid: countryIsValid,
+    hasError: countryHasError,
+    valueChangeHandler: countryChangeHandler,
+    inputBlurHandler: countryBlurHandler,
+    reset: resetCountry,
+  } = useInput(isNotEmpty);
+  const {
+    value: messageValue,
+    isValid: messageIsValid,
+    hasError: messageHasError,
+    valueChangeHandler: messageChangeHandler,
+    inputBlurHandler: messageBlurHandler,
+    reset: resetMessage,
+  } = useInput(isNotEmpty);
+
+  let formIsValid = false;
+
+  if (firstNameIsValid && lastNameIsValid && emailIsValid && phoneNumberIsValid && jobTitleIsValid && organizationIsValid && countryIsValid && messageIsValid) {
+    formIsValid = true;
   }
-});
 
-const applicationFields = applicationFieldsInfo.map((field) => {
-  return (
-    <Grid item xs={12} md={6}>
-      <InputTextField required fullWidth id={field.id} label={field.label} variant="filled" size="small"/>
-    </Grid>
-  )
-})
+  const submitHandler = event => {
+    event.preventDefault();
+    console.log('Submitted!');
+    console.log(firstNameValue, lastNameValue, emailValue, phoneNumberValue, jobTitleValue, organizationValue, countryValue, messageValue);
 
-const ApplicationForm = () => {
+    if (!formIsValid) {
+      return;
+    }
 
+    console.log('Validated!');
+    console.log(firstNameValue, lastNameValue, emailValue, phoneNumberValue, jobTitleValue, organizationValue, countryValue, messageValue);
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+    resetPhoneNumber();
+    resetJobTitle();
+    resetOrganization();
+    resetCountry();
+    resetMessage();
+  };
+
+  const InputTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: theme.palette.common.lightBlue,
+    },
+    "&:hover label": {
+      color: theme.palette.common.lightBlue
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'white',
+      },
+      '&:hover fieldset': {
+        borderColor: theme.palette.common.lightBlue,
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: theme.palette.common.lightBlue,
+      },
+    }
+  });
 
   return (
     <div className={classes.form}>
       <h2>Get In Touch</h2>
-      <Grid container columnSpacing={6} rowSpacing={4}>
-        {applicationFields}
-        <Grid item xs={12} md={12}>
-          <InputTextField
-            required 
-            fullWidth 
-            id="filled-textarea"
-            label="How do you see the Fintechport Lab helping you?"
-            multiline
-            variant="filled"
-            rows={4}
-            sx={{marginBottom: "5%"}}
-          />
+      <form onSubmit={submitHandler}>
+        <Grid container columnSpacing={6} rowSpacing={4}>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              error={firstNameHasError}
+              helperText={firstNameHasError ? "Please enter your name" : ""}
+              required 
+              fullWidth 
+              id="First Name"
+              label="First Name" 
+              value={firstNameValue} 
+              onChange={firstNameChangeHandler} 
+              onBlur={firstNameBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              error={lastNameHasError}
+              helperText={lastNameHasError ? "Please enter your surname" : ""}
+              required 
+              fullWidth 
+              id="Last Name"
+              label="Last Name" 
+              value={lastNameValue} 
+              onChange={lastNameChangeHandler} 
+              onBlur={lastNameBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              error={emailHasError}
+              helperText={emailHasError ? "Please enter a valid email address" : ""}
+              required 
+              fullWidth 
+              id="Email"
+              label="Email" 
+              value={emailValue} 
+              onChange={emailChangeHandler} 
+              onBlur={emailBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              error={phoneNumberHasError}
+              helperText={phoneNumberHasError ? "Please enter a valid phone number" : ""}
+              required 
+              fullWidth 
+              id="Phone Number"
+              label="Phone Number" 
+              value={phoneNumberValue} 
+              onChange={phoneNumberChangeHandler} 
+              onBlur={phoneNumberBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              error={jobTitleHasError}
+              helperText={jobTitleHasError ? "Please enter your job title" : ""}
+              required 
+              fullWidth 
+              id="Job Title"
+              label="Job Title" 
+              value={jobTitleValue} 
+              onChange={jobTitleChangeHandler} 
+              onBlur={jobTitleBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField 
+              error={organizationHasError}
+              helperText={organizationHasError ? "Please enter your organization" : ""}
+              fullWidth 
+              id="Organization"
+              label="Organization" 
+              value={organizationValue} 
+              onChange={organizationChangeHandler} 
+              onBlur={organizationBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              error={countryHasError}
+              helperText={countryHasError ? "Please enter your country" : ""}
+              required 
+              fullWidth 
+              id="Country"
+              label="Country" 
+              value={countryValue} 
+              onChange={countryChangeHandler} 
+              onBlur={countryBlurHandler} 
+              variant="filled" 
+              size="small"
+            />
+          </Grid>
+            {/* {applicationFields} */}
+          <Grid item xs={12} md={12}>
+            <TextField
+              error={messageHasError}
+              helperText={messageHasError ? "Message must be thirty or more characters" : ""}
+              required 
+              fullWidth 
+              id="filled-textarea"
+              label="How do you see the Fintechport Lab helping you?"
+              multiline
+              variant="filled"
+              rows={4}
+              value={messageValue}
+              onChange={messageChangeHandler}
+              onBlur={messageBlurHandler}
+              sx={{marginBottom: "5%"}}
+            />
+          </Grid>
         </Grid>
-      </Grid>
-      <ContainedButtonEl action={"Submit"} />
+        <ContainedButtonEl type={"submit"} action={"Submit"} />
+      </form>
     </div>
   )
 };
